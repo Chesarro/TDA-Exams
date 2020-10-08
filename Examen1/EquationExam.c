914ee34cb98a875c0ac6ba943451df9c5aa7fc3e
MirnaCardona <69806001+MirnaCardona@users.noreply.github.com>
2020-10-08 17:04:17 -0500

    Create EquationExam.c

---
+++ Examen1/EquationExam.c
@@ -0,0 +1,21 @@
+#include "EquationExam.h"
+#include "ExamMain.h"
+#include "files.h"
+
+/**
+ * Obtiene el resultado de la ecuacion de las fuerzas del sistema.
+ * @param
+ *  masa (double):
+ *      La masa del usuario en Kg. 
+ *  Dt (double ):
+ *      nuestra constante de cambio
+ *  k (double):
+ *      la constante de nuestro bungee
+ *
+ * @return double.
+*/
+void calculaVC(double arreglo[], double *media, double *mediana, double *moda)
+{
+  
+}
+
