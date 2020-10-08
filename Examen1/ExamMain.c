8208abb479196b5ff3f2b1fe1dc77d1418896991
MirnaCardona <69806001+MirnaCardona@users.noreply.github.com>
2020-10-08 17:30:12 -0500

    Update ExamMain.c

--- Examen1/ExamMain.c
+++ Examen1/ExamMain.c
@@ -12,13 +12,13 @@ int main(void){
     gets(nombre);
     arch1 = fopen(nombre,"r");
   fscanf(arch1, "%s", nombre);
-  MAX=50;
-    for(i=1;i<=MAX;i++)
+  MAX=49;
+    for(i=0;i<=MAX;i++)
     {fscanf(arch1, "%d,%f", &indice, &valor);
      //printf("%d%f\n",indice, valor);
     valorestabla[i]=valor;
     }
-     for(i=1;i<=MAX;i++)
+     for(i=0;i<=MAX;i++)
     {printf("%d,%f\n",i, valorestabla[i] );
     
     }
