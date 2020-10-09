b5146521665899d6bdf074499c03fb3c11ee0fa0
MirnaCardona <69806001+MirnaCardona@users.noreply.github.com>
2020-10-08 21:20:28 -0500

    Update ExamMain.c

--- Examen1/ExamMain.c
+++ Examen1/ExamMain.c
@@ -6,22 +6,26 @@ int MAX;
 int main(void){
   char nombre[100];
   int indice,i;
-  float valor, valorestabla[100], media, mediana, moda, desviacion, distancia;
+  float valor, *valorestabla, media, mediana, moda, desviacion, distancia;
     FILE * arch1, *arch2;
     printf("Dime el nombre del archivo\n");
     gets(nombre);
     arch1 = fopen(nombre,"r");
   fscanf(arch1, "%s", nombre);
-  MAX=49;
+  printf("Dime el numero de tus datos\n");
+  fscanf("%d",&MAX); 
+  
+//  MAX=49;
+  *valorestabla=(float*)malloc(sizeof(float)*MAX);
     for(i=0;i<=MAX;i++)
     {fscanf(arch1, "%d,%f", &indice, &valor);
-     printf("%d%f\n",indice, valor);
+     //printf("%d%f\n",indice, valor);
     valorestabla[i]=valor;
     }
-     for(i=0;i<=MAX;i++)
+     /*for(i=0;i<=MAX;i++)
     {printf("%d,%f\n",i, valorestabla[i] );
     
-    }
+    }*/
   calculaVC(valorestabla, MAX, &media, &mediana, &moda);
    calculaVD(valorestabla, MAX, mediana, &desviacion, &distancia);
    
