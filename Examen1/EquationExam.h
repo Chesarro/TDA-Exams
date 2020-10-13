c2204b2691035b8f266cf66602fcca29cae6e3ef
MirnaCardona <69806001+MirnaCardona@users.noreply.github.com>
2020-10-13 18:08:36 -0500

    Update EquationExam.h

--- Examen1/EquationExam.h
+++ Examen1/EquationExam.h
@@ -31,23 +31,6 @@
 /* Function prototypes. */
 
 
-/*
- *
- * La funcion equations_fuezas calcula la fuerza externa
- *
- * @params
- *      masa (double):
- * masa proporcionada por el usuario dado por el usuario 
- *      Dt(double); 
- *   diferencia de tiempo
- *    K (double):
- *  Constante del bongee
-.*     xt (double)
- *   posicion 2 segundos atras atras
- * @returns
-  int
-*/
-
 
 EXTERN void calculaVC(float *valorestabla, int MAX, float *media, float *mediana, float *moda);
 EXTERN void calculaVD(float *valorestabla,int MAX, float mediana, float *desviacion, float *distancia);
